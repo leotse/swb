0a261de78832b56b999014a0c5b4e3e7eb3f3c14
Leo Tse <leocltse@gmail.com>
2014-09-26 19:00:06 -0400

    Added split adjusted prices

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ function script(done) {
   log.debug('starting test script');
 
   var stock = data.get('aapl');
-  var filtered = stock.interval('2014-08-01', '2014-08-31');
+  var filtered = stock.interval('2014-05-01', '2014-06-30');
 
   _.each(filtered, function(d) { 
     console.log('%s %s %s %s% %s', 
       d.date.format('YYYY-MM-DD'),
       d.open.toFixed(2), 
-      d.close.toFixed(2), 
-      d.change.toFixed(2), 
-      d.sma50.toFixed(2)
+      d.close.toFixed(2)
+      // d.change.toFixed(2), 
+      // d.sma50.toFixed(2)
     ); 
   });
 
--- data/index.js
+++ data/index.js
@@ -21,6 +21,8 @@ var indicators = require('./indicators');
 
 // constants
 var DATE_FORMAT = 'YYYY-MM-DD';
+var DATE_FORMAT2 = 'YYYYMMDD';
+var SPLIT_TAG = 'SPLIT';
 
 
 // public - initialize the module
@@ -30,32 +32,56 @@ module.exports.init = function(callback) {
 
   // get a list of available data files
   var dir = path.join(__dirname, './raw');
-  var files = _.chain(fs.readdirSync(dir))
-    .filter(function(f) { return f.indexOf('.csv') >= 0 })
-    .map(function(f) { return path.join(dir, f); })
-    .value();
+  var dirfiles = fs.readdirSync(dir);
+  var files = _.chain(dirfiles).reduce(function(memo, file) {
+    var doti = file.indexOf('.');
+    var ticker = file.substring(0, doti);
+    var ext = file.substring(doti + 1);
+    if(ext === 'csv' || ext === 'ds') {
+      if(!memo[ticker]) { memo[ticker] = { ticker: ticker }; }
+      memo[ticker][ext] = path.join(dir, file);
+    }
+    return memo;
+  }, {}).toArray().value();
 
-  async.eachSeries(files, function(file, done) {
+  // start processing csv and ds files for each ticker
+  async.eachSeries(files, function(file, fileDone) {
 
     // init stock object for this file
-    var ticker = file.match(/([^\/\.]+)\.csv/i)[1];
-    var stock =  new Stock(ticker);
-    data[ticker] = stock;
+    var stock =  new Stock(file.ticker);
+    data[file.ticker] = stock;
 
-    // input data streams
-    var istream = fs.createReadStream(file);
-    var rl = readline.createInterface({ 
-      input: istream, 
-      terminal: false 
-    });
+    async.series([
+      function(done) {
+        // input data streams
+        var istream = fs.createReadStream(file.csv);
+        var rl = readline.createInterface({ 
+          input: istream, 
+          terminal: false 
+        });
 
-    // read file!
-    rl.on('line', function(line) { stock.parse(line); });
-    rl.on('close', function() { 
-      _.each(indicators, function(apply) { apply(stock); });
-      setImmediate(done); 
-    });
+        // read file!
+        rl.on('line', function(line) { stock.parsePrice(line); });
+        rl.on('close', done);
+      },
+      function(done) {
+        // input data streams
+        var istream = fs.createReadStream(file.ds);
+        var rl = readline.createInterface({ 
+          input: istream, 
+          terminal: false 
+        });
 
+        // read file!
+        rl.on('line', function(line) { stock.parseSplit(line); });
+        rl.on('close', done);
+
+      },
+      function(done) {
+        stock.commit();
+        setImmediate(done);
+      }
+    ], fileDone);
   }, callback);
 };
 
@@ -68,14 +94,16 @@ module.exports.get = function(ticker) { return data[ticker]; };
 function Stock(ticker) {
   var self = this;
   var series = [];
+  var splits = [];
+  var initialized = false;
 
   // for init only, parses and stores a line of data
-  self.parse = function(line) {
+  self.parsePrice = function(line) {
     var parts = line.split(',');
-    if(parts.length !== 7) { return log.error('invalid line: %s', line); }
+    if(parts.length !== 7) { return log.error('invalid price line: %s', line); }
 
     var date = moment.utc(parts[0], DATE_FORMAT);
-    if(!date.isValid()) { return log.error('invalid line: %s', line); }
+    if(!date.isValid()) { return log.error('invalid price line: %s', line); }
 
     var open = Number(parts[1]), high = Number(parts[2]), low = Number(parts[3]),
       close = Number(parts[4]), volume = Number(parts[5]), adjClose = Number(parts[6]);
@@ -89,13 +117,68 @@ function Stock(ticker) {
       volume: volume,
       adjClose: adjClose
     });
+  };
+
+  // for init only, parses and split adjust prices
+  // needs to be called after prices are parsed
+  self.parseSplit = function(line) {
+    var parts = line.split(',');
+    if(parts.length !== 3) { return log.error('invalid split line: %s', line); }
+
+    var date = moment.utc(parts[1], DATE_FORMAT2);
+    if(!date.isValid()) { return log.error('invalid split line: %s', line); }
+
+    if(parts[0] === SPLIT_TAG) {
+      var factorParts = parts[2].split(':');
+      var factor = Number(factorParts[1]) / Number(factorParts[0]);
+      splits.push({
+        date: date,
+        factor: factor
+      });
+    }
+  };
+
+  self.commit = function() {
+
+    var prices = self.series();
+
+    // split adjust stock prices
+    if(splits.length > 0) {
+      var i = 0, j = 0, factor = 1;
+      var price, split = splits[0];
+      while(i < prices.length) {
+        price = prices[i++];
+        price.open = price.open * factor;
+        price.high = price.high * factor;
+        price.low = price.low * factor;
+        price.close = price.close * factor;
+
+        if(split && price.date.diff(split.date) === 0) {
+          split = splits[j++];
+          factor = factor * split.factor;
+        }
+      }
+    }
+
+    // init completed
+    initialized = true;
   }
 
   // return the entire series
-  self.series = function() { return series; }
+  self.series = function() { 
+    // ensureInitialized();
+    return series; 
+  };
+
+  // return the stock splits data
+  self.splits = function() {
+    // ensureInitialized();
+    return splits;
+  }
 
   // return an interval, inclusive start and end parameter
   self.interval = function(start, end) {
+    ensureInitialized();
     start = moment.utc(start, DATE_FORMAT);
     end = moment.utc(end, DATE_FORMAT);
     if(!start.isValid() || !end.isValid()) { throw new Error('invalid start or end date, dates must be in format: ' + DATE_FORMAT); }
@@ -105,5 +188,11 @@ function Stock(ticker) {
       }
       return false;
     });
+  };
+
+  // helper - ensure stock is initialized properly
+  function ensureInitialized() {
+    if(!initialized) { throw new Error('stock ' + ticker + ' is not initialized'); }
+    return true;
   }
 }
